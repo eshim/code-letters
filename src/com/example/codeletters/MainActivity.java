d8641534dc0c85c1e05daccb5a84e5ecf0e937cf
eshim <eugenejshim>
2014-04-07 15:04:00 -0400

    adapter notification + cleanup

--- src/com/example/codeletters/MainActivity.java
+++ src/com/example/codeletters/MainActivity.java
@@ -22,15 +22,12 @@ public class MainActivity extends Activity {
 	private Map<String, String> listWU = new HashMap<String, String>(); // Dictionary of Western Union Phonetic Letters
 	private ArrayList<Card> letterCardList = new ArrayList<Card>(); // List of Cards to scroll through
 	private CardScrollView letterCardListScrollView;
+	private ExampleCardScrollAdapter adapter;
 
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		
-		
-		
 		
 		listWU.put("A", "Adams"); 
     	listWU.put("B", "Boston");
@@ -60,50 +57,15 @@ public class MainActivity extends Activity {
     	listWU.put("Z", "Zero");
     	// it would be better to store this dictionary somewhere instead of
     	// inputting values every time this program starts
-    	createCards();
     	displaySpeechRecognizer();
     
-    	// transcribed text from voice prompt instead of activity
-		/*List<String> voiceResults = getIntent().getExtras()
-				.getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
-
-        String capturedWord = voiceResults.get(0);
-		Card letterCard;
-        
-        for (int i = 0; i < capturedWord.length(); i++){
-        	
-        	char capturedLetter = Character.toUpperCase(capturedWord.charAt(i));
-        	// want to match it to uppercase keys
-        	String phoneticLetter = listWU.get(Character.toString(capturedLetter));
-        	
-        	letterCard = new Card(this);
-        	letterCard.setText(phoneticLetter);
-        	letterCardList.add(letterCard);
-        }*/
-    			
-    	
-    	// lifted wholesale from https://developers.google.com/glass/develop/gdk/ui/theme-widgets
     	letterCardListScrollView = new CardScrollView(this);
-    	ExampleCardScrollAdapter adapter = new ExampleCardScrollAdapter();
+    	adapter = new ExampleCardScrollAdapter();
     	letterCardListScrollView.setAdapter(adapter);
     	letterCardListScrollView.activate();
     	setContentView(letterCardListScrollView);
-    	
-//        setContentView(R.layout.activity_main);
-        
-        
-//        if (savedInstanceState == null) {
-//            getFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment())
-//                    .commit();
-//        } 
     }
 	
-	private void createCards() { // ADDED
-		Card instructionCard = new Card(this);
-		instructionCard.setText("Swipe forward for phonetic spelling.");
-		letterCardList.add(instructionCard);
-	  }
 
     private static final int SPEECH_REQUEST = 0;
 
@@ -112,42 +74,35 @@ public class MainActivity extends Activity {
         startActivityForResult(intent, SPEECH_REQUEST);
     }
 
+    /** 
+     * onActivityResult()
+     * Records spoken text and breaks each recorded word down by character. For
+	 * each letter, a corresponding phonetic letter card is added to the 
+	 * letterCardList. Finally the adapter is notified of the updated data.
+	 */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	// Takes spoken text and turns each letter into a card
-    	
         if (requestCode == SPEECH_REQUEST && resultCode == RESULT_OK) {
             List<String> results = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
             String capturedWord = results.get(0);
-
             Card letterCard;
             
             for (int i = 0; i < capturedWord.length(); i++){
-            	
             	char capturedLetter = Character.toUpperCase(capturedWord.charAt(i));
-            	// want to match it to uppercase keys
             	String phoneticLetter = listWU.get(Character.toString(capturedLetter));
             	
             	letterCard = new Card(this);
             	letterCard.setText(phoneticLetter);
             	letterCardList.add(letterCard);
-            	
-            	
             }
-            // break word into characters
-            // add each matching value to a list
-            // match each character as a key to the dictionary
-            // put each list entry into a card
-            // move to next card by either pronouncing the word displayed or swiping
-            // end card is "Again?"
+            adapter.notifyDataSetChanged();
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
     
     
     private class ExampleCardScrollAdapter extends CardScrollAdapter {
-    	// lifted wholesale from https://developers.google.com/glass/develop/gdk/ui/theme-widgets
 
         @Override
         public int findIdPosition(Object id) {
@@ -174,40 +129,4 @@ public class MainActivity extends Activity {
             return letterCardList.get(position).toView();
         }
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    /**
-//     * A placeholder fragment containing a simple view.
-//     */
-//    public static class PlaceholderFragment extends Fragment {
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//            return rootView;
-//        }
-//    }
-//
 }
